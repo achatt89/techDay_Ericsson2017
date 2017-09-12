b87286f7f2dfeae2637566ef222189a5cf378b59
echeabt <abhijit.chatterjee@ericsson.com>
2017-09-12 16:17:58 +0100

    Project Upload from Local

---
+++ Assets/BrokenNodeAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class BrokenNodeAction : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject brokenNodeActionObject;
+
+    // Use this for initialization
+	void Start () {
+        brokenNodeActionObject = GameObject.Find("brokenNode_Btn");
+        brokenNodeActionObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("TADAA!!!!!!!!!!!!!!!!");
+        Application.OpenURL("http://www.luisbravoa.com/brand-prototype");
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+      
+    }
+
+}
